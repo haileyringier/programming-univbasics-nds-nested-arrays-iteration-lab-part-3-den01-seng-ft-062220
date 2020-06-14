f968054944f5b0fb5170c98ba21124b8c545879f
Hailey Ringier <haileyringier@outlook.com>
2020-06-14 15:40:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,6 @@ def join_nested_strings(src)
   row_index = 0 
   while row_index < src.count do
     element_index = 0
-    final_sentence = []
     if src[row_index][element_index].class == String
       final_sentence << src[row_index][element_index]
     end
